2e6d370b145378c23505aa5fb52ccb4385c5e3a7
gromgrad <snegobur@gmail.com>
2024-11-16 21:16:10 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,15 @@
+#подсчет символов в строке1
+my_string=("Сегодня лучший день:")
+print=input(len(my_string))
+
+#вывод в верхний и нижний регистр
+print=input(my_string.upper())
+print=input(my_string.lower())
+#удаление пробелов
+print=input(my_string.replace(' ',''))
+# вывод первого символа строки
+print=input(my_string[0])
+# вывод последнего символа строки
+print=input(my_string[19])
+
+
